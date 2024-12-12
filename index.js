8d0e1068cd1d714d19ab3a919c4912a713bca442
Leonit Ahmeti <leonit.ahmeti@bluewin.ch>
2024-12-12 09:25:41 +0100

    linter fixed

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 const chalk = require('chalk');
 
-// Fehler 1: 'myUnusedVariable' wird zugewiesen, aber nie verwendet
-let myUnusedVariable = 'This is an unused variable';
-
-// Fehler 2: 'myUnusedVariable' sollte als 'const' deklariert werden
-myUnusedVariable = 'Reassigned value';
-
-// Fehler 3: Nutzung von '==' statt '==='
-if (myUnusedVariable == 'Reassigned value') {
-  console.log(chalk.green('It works'));
-}
+console.log(chalk.green('It works'));
